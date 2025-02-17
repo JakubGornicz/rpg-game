e5955f6b4c7944108ed006dd4fac328fe87d7d2c
JakubGornicz <gorniczjakub@gmail.com>
2025-02-17 08:49:10 +0100

    Added Player Movement
    - cleaned up code
    - added player spritesheet
    - added player movement

--- LearningSFML/main.cpp
+++ LearningSFML/main.cpp
@@ -4,54 +4,32 @@
 
 int main()
 {
-#pragma region random
-
-    sf::Time t1 = sf::milliseconds(1000);
-    sf::Time t2 = sf::seconds(5);
-
-    sf::Clock clock;
-    // clock.start();
-#pragma endregion
-
+#pragma region window
     // initialise variables
-    unsigned int width = 1200;
-    unsigned int height = 800;
+    unsigned int windowWidth = 960;
+    unsigned int windowHeight = 640;
     sf::ContextSettings settings; 
     settings.antialiasingLevel = 8;
-    sf::RenderWindow window(sf::VideoMode({ width, height }), "SFML shapes", sf::Style::Default, settings);
-
-    float size = std::abs(50.0f);
-    sf::CircleShape circle(size);
-    circle.setOrigin(sf::Vector2f(circle.getRadius(), circle.getRadius()));
-    circle.setFillColor(sf::Color::Yellow);
-    circle.setPosition(sf::Vector2f(width / 2, height / 2));
-
-
-    sf::RectangleShape rectangle(sf::Vector2f(100, 50));
-    rectangle.setOrigin(sf::Vector2f(rectangle.getSize() / 2.0f));
-    rectangle.setFillColor(sf::Color::Green);
-    rectangle.setPosition(sf::Vector2f(width / 2, height / 2 + 200));
-    rectangle.setRotation(45);
-
-    sf::CircleShape triangle(80.0f, 3);
-    triangle.setOrigin(sf::Vector2f(triangle.getRadius(), triangle.getRadius()));
-    triangle.setFillColor(sf::Color::Red);
-    triangle.setPosition(sf::Vector2f(width / 2, height / 2 - 200));
-    triangle.setRotation(90);
-
-    sf::CircleShape square(80.0f, 4);
-    square.setOrigin(sf::Vector2f(square.getRadius(), square.getRadius()));
-    square.setFillColor(sf::Color::Blue);
-    square.setPosition(sf::Vector2f(width / 2 + 200, height / 2));
-    //square.setRotation(sf::degrees(45));
-
-    sf::CircleShape sextagon(80.0f, 6);
-    sextagon.setOrigin(sf::Vector2f(sextagon.getRadius(), sextagon.getRadius()));
-    sextagon.setFillColor(sf::Color::Magenta);
-    sextagon.setPosition(sf::Vector2f(width / 2 - 200, height / 2));
-
-
-    // main game loop
+    sf::RenderWindow window(sf::VideoMode({ windowWidth, windowHeight }), "SFML shapes", sf::Style::Default, settings);
+#pragma endregion
+#pragma region load textures
+    sf::Texture Playertexture;
+    sf::Sprite playerSprite;
+    if (Playertexture.loadFromFile("Assets/Player/Textures/spritesheet.png"))
+    {
+        std::cout << "Player texture loaded!" << std::endl;
+	    playerSprite.setTexture(Playertexture);
+        int xIndex = 0;
+        int yIndex = 0;
+		playerSprite.setTextureRect(sf::IntRect(xIndex * 64, yIndex * 64, 64, 64));
+        playerSprite.scale(sf::Vector2f(5.0f, 5.0f));
+    }
+    else
+    {
+		std::cout << "Player texture failed to load!" << std::endl;
+    }
+#pragma endregion
+#pragma region main game loop
     while (window.isOpen())
     {
         // handle window events 
@@ -62,16 +40,44 @@ int main()
                 window.close();
         }
 
+        int temp = 5*64;
+        // handle user input
+        sf::Vector2f currentPosition = playerSprite.getPosition();
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        {
+            if (currentPosition.x < windowWidth - temp)
+            {
+                playerSprite.setPosition(currentPosition + sf::Vector2f(1, 0));
+            }
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            if (currentPosition.x > 0)
+            {
+                playerSprite.setPosition(currentPosition + sf::Vector2f(-1, 0));
+            }
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+        {
+            if (currentPosition.y > 0)
+            {
+                playerSprite.setPosition(currentPosition + sf::Vector2f(0, -1));
+            }
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+        {
+            if (currentPosition.y < windowHeight - temp)
+            {
+                playerSprite.setPosition(currentPosition + sf::Vector2f(0, 1));
+            }
+        }
+
         window.clear(sf::Color::Black);
 
-        window.draw(circle);
-        window.draw(rectangle);
-        window.draw(triangle);
-        window.draw(square);
-        window.draw(sextagon);
+        window.draw(playerSprite);
         window.display();
 
     }
-
+#pragma endregion
     return 0;
 }
